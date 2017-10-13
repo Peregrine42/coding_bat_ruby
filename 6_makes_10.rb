960201a1f81c9d0b5537e6129f7b09fa82b6264a
Duncan Fermor (TNK6) <duncan.fermor@bt.com>
2017-10-13 14:19:30 +0100

    Task 6 complete.

---
+++ 6_makes_10.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Makes 10
+#
+# Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+# 
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes_10(a, b)
+  # Please write your code in here
+end
+
+# --- PLEASE DO NOT WRITE ANY CODE BELOW THIS LINE ---
+
+puts <<~EOF
+  Expected output:
+  true
+  false
+  true
+
+  Actual output:
+EOF
+
+puts makes_10(9, 10)
+puts makes_10(9, 9)
+puts makes_10(1, 9)
